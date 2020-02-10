83cf05744bdf99d2b89b41f3bebba540f6315cef
Filipe Veber <FilipeVeber@users.noreply.github.com>
2020-02-10 17:56:11 -0300

    Atualizar contato

--- lib/helper/contact.dart
+++ lib/helper/contact.dart
@@ -68,6 +68,12 @@ class ContactHelper {
     return await dbContact
         .delete(CONTACT_TABLE, where: "$ID_COLUMN = $id", whereArgs: [id]);
   }
+
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(CONTACT_TABLE, contact.toMap(),
+        where: "$ID_COLUMN = ?", whereArgs: [contact.id]);
+  }
 }
 
 class Contact {
