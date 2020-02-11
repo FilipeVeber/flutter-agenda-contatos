1ce3801ea7feb06c5f0811dc26ab603bc085dd8e
Filipe Veber <FilipeVeber@users.noreply.github.com>
2020-02-11 11:07:23 -0300

    ContactPage

---
+++ lib/UI/contact_page.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_agenda_contatos/helper/contact.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  Contact _editedContact;
+
+  bool _userEdited = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.contact == null) {
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
+    }
+  }
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          _editedContact.name ?? "Novo contato",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                height: 140,
+                width: 140,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: _editedContact.image != null
+                            ? FileImage(File(_editedContact.image))
+                            : AssetImage("images/person.png"))),
+              ),
+            ),
+            TextField(
+              controller: _nameController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (value) {
+                _userEdited = true;
+                setState(() {
+                  _editedContact.name = value;
+                });
+              },
+            ),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(labelText: "E-mail"),
+              onChanged: (value) {
+                _userEdited = true;
+                _editedContact.email = value;
+              },
+            ),
+            TextField(
+              controller: _phoneController,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(labelText: "Fone"),
+              onChanged: (value) {
+                _userEdited = true;
+                _editedContact.phone = value;
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Icon(Icons.check),
+        onPressed: () {},
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,4 +3,5 @@ import 'package:flutter_agenda_contatos/UI/home_page.dart';
 
 void main() => runApp(MaterialApp(
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     ));
